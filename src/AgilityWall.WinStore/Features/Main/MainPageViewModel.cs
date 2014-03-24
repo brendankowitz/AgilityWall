8fd42ccf6a856b4da3bc9e3ebbae592c9ad1d850
Brendan Kowitz <brendan.kowitz@gmail.com>
2014-03-25 07:43:55 +1000

    adding caliburn packages

---
+++ src/AgilityWall.WinStore/Features/Main/MainPageViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Caliburn.Micro;
+
+namespace AgilityWall.WinStore.Features.Main
+{
+    public class MainPageViewModel : PropertyChangedBase
+    {
+        public MainPageViewModel()
+        {
+        }
+    }
+}
