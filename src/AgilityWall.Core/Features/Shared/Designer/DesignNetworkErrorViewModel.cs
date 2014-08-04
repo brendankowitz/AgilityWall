e9c1b645eb34027a66293145fb5f8998c9a4b45e
Brendan Kowitz <brendan.kowitz@gmail.com>
2014-08-05 08:21:41 +1000

    hide network error view at designtime

---
+++ src/AgilityWall.Core/Features/Shared/Designer/DesignNetworkErrorViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Caliburn.Micro;
+
+namespace AgilityWall.Core.Features.Shared.Designer
+{
+    public class DesignNetworkErrorViewModel : NetworkErrorViewModel
+    {
+        public DesignNetworkErrorViewModel() : base(null)
+        {
+            ShowOfflineStatus = false;
+        }
+    }
+}
