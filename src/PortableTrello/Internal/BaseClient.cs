dddbaa298784524a9da8d53b85670e0cea3c7209
Brendan Kowitz <brendan.kowitz@gmail.com>
2014-08-04 22:03:43 +1000

    changed name to agility wall reader (while it only reads things)

--- src/PortableTrello/Internal/BaseClient.cs
+++ src/PortableTrello/Internal/BaseClient.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
@@ -72,7 +73,8 @@ namespace PortableTrello.Internal
                     AllowAutomaticDecompression = false;
                 }
             }
-            return new HttpClient(handler);
+            var httpClient = new HttpClient(handler);
+            return httpClient;
         }
 
         protected virtual Task<string> HandleResponseMessage(HttpResponseMessage message)
