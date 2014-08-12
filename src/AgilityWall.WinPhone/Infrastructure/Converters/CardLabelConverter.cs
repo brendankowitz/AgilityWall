0535d8fdab0be20067c5d730711fda3e7d796673
Brendan Kowitz <brendan.kowitz@gmail.com>
2014-08-12 21:05:30 +1000

    renders labels on cards

---
+++ src/AgilityWall.WinPhone/Infrastructure/Converters/CardLabelConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace AgilityWall.WinPhone.Infrastructure.Converters
+{
+    public class CardLabelConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var key = string.Format("CardLabel{0}", TitleCase(value as string));
+
+            if (App.Current.Resources.Contains(key))
+                return App.Current.Resources[key];
+
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        string TitleCase(string input)
+        {
+            if (string.IsNullOrEmpty(input)) return input;
+            return input[0].ToString().ToUpper() + input.Substring(1);
+        }
+    }
+}
---
+++ src/AgilityWall.WinStore/Infrastructure/Converters/CardLabelConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+using Windows.UI.Xaml.Data;
+
+namespace AgilityWall.WinStore.Infrastructure.Converters
+{
+    public class CardLabelConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            var key = string.Format("CardLabel{0}", TitleCase(value as string));
+
+            if (App.Current.Resources.ContainsKey(key))
+                return App.Current.Resources[key];
+
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+
+        string TitleCase(string input)
+        {
+            if (string.IsNullOrEmpty(input)) return input;
+            return input[0].ToString().ToUpper() + input.Substring(1);
+        }
+    }
+}
