78eb61125fc713ac202bc83c78fe8c39d9b924f6
Brendan Kowitz <brendan.kowitz@gmail.com>
2015-01-21 07:59:12 +1000

    added card date formatter

---
+++ src/AgilityWall.WinPhone/Infrastructure/Converters/CommentDateFormatter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace AgilityWall.WinPhone.Infrastructure.Converters
+{
+    public class CommentDateFormatter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            try
+            {
+                var date = System.Convert.ToDateTime(value);
+                var format = date.ToLocalTime().ToString("h:mmtt MMM d");
+                if(date.Year != DateTime.Today.Year)
+                    format += date.ToLocalTime().ToString(" yyyy");
+                return format;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
